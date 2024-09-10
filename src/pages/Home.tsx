5a02c3538492a1b05c5eca367e448d5100b2186d
f776 <fern.belmar@duocuc.cl>
2024-09-10 16:58:52 -0300

    ready to use

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,6 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
-import "./Home.css";
 
 const Home: React.FC = () => {
   return (
